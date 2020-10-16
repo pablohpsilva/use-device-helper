89f5ad7fdf68e60038902b2b9f2d0ebf2d47e2c6
Pablo Henrique Penha Silva <pablo.silva@doclerholding.com>
2020-10-16 16:20:53 +0200

    chore: trying to use bowser

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,19 @@
 import { useMemo } from "react";
+import Parser from "bowser/src/parser";
 import useBreakpoint from "@w11r/use-breakpoint";
 
-export enum Devices {
-  IOS = "IOS",
-  ANDROID = "ANDROID",
-  WINDOWSPHONE = "WP",
-  SYMBIAN = "SYMBIAN",
-}
-
-export enum AppleDevices {
-  iPhone4 = 480,
-  iPhoneSE = 568,
-  iPhone8 = 667,
-  iPhonePlus = 736,
-  iPhoneX = 812,
-  iPhoneMax = 896,
-}
-
-export enum SamsungDevices {
-  GalaxyS3 = "Galaxy S III",
-  GalaxyS4 = "Galaxy S4",
-  GalaxyS5 = "Galaxy S5",
-  SamsungNote2 = "Galaxy Note II",
-  GalaxyNote3 = "Galaxy Note 3",
-  GalaxyNote4 = "Galaxy Note 4",
-  GalaxyTabPro101 = "Galaxy TabPro 10.1",
-}
-
-export enum Browsers {
-  AmazonSilk = "Silk",
-  Android = "Android Browser",
-  BLine = "B-Line",
-  DBrowser = "dbrowser",
-  Dolphin = "Dolphin",
-  Firefox = "Firefox",
-  InternetExplorer = "Internet Explorer",
-  Isivioo = "Isivioo",
-  MIUI = "MIUI Browser",
-  Mercury = "Mercury",
-  Opera = "Opera",
-  Safari = "Safari",
-  SafariMobile = "Mobile Safari",
-  Samsung = "Samsung Browser",
-  Silk = "Silk",
-  SilkMobile = "Mobile Silk",
-  UC = "UC Browser",
-  Yandex = "Yandex Browser",
-}
+console.log("Parser", Parser);
 
 const useDeviceBreakpoints = () => {
   const breakpointsChanges = useBreakpoint();
+  const browser = new Parser(window.navigator.userAgent).getResults();
 
-  return useMemo(
-    () => ({
-      ...breakpointsChanges,
-    }),
-    [breakpointsChanges]
-  );
+  const deviceBreakpointsValue = {
+    ...breakpointsChanges,
+    browser,
+  };
+
+  return useMemo(() => deviceBreakpointsValue, [breakpointsChanges]);
 };
 
 export default useDeviceBreakpoints;
