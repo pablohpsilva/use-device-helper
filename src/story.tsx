1d6f288b3cfa312dcb96a05861ab12d5f462c179
Pablo Henrique Penha Silva <pablo.silva@doclerholding.com>
2021-05-21 13:48:53 +0200

    feat(storybook): created storybook based on useBreakpoints package

---
+++ src/story.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useDeviceBreakpoints } from "./hook";
+import { DeviceBreakpointProvider } from "./provider";
+import "./story.css";
+
+const Component = () => {
+  const value = useDeviceBreakpoints(
+    "none",
+    [
+      ["micro", "is micro"],
+      ["mobile", "is mobile"],
+      ["tablet", "is tablet"],
+      ["small", "is small"],
+      ["medium", "is medium"],
+      ["large", "is large"],
+    ],
+    {}
+  );
+
+  console.log(value);
+
+  return (
+    <div className="wrapper">
+      Example code
+      <pre>
+        {`const value = useDeviceBreakpoints('none', [
+      ['micro', 'is micro'],
+      ['mobile', 'is mobile'],
+      ['tablet', 'is tablet'],
+      ['small', 'is small'],
+      ['medium', 'is medium'],
+      ['large', 'is large']
+  ],
+  {macos: {chrome: '>80'}})`}
+      </pre>
+      <div>`{value?.value}` is the current value. Resize to change.</div>
+      <p>
+        <span>
+          {value?.platform?.vendor} - {value?.platform?.type}
+        </span>
+        <br />
+        <span>
+          {value?.browser?.name} - v{value?.browser?.version}
+        </span>
+        <br />
+        <span>{value?.engine?.name}</span>
+        <br />
+        <span>
+          {value?.os?.name} - v{value?.os?.version} - {value?.os?.versionName}
+        </span>
+        <br />
+        <br />
+        <span>
+          Is Chrome MacOS({`{ macos: { chrome: ">80" } }`})?{" "}
+          {value?.satisfies ? "true" : "false"}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default {
+  title: "useDeviceBreakpoints",
+  component: Component,
+  decorators: [
+    (fn) => <DeviceBreakpointProvider>{fn()}</DeviceBreakpointProvider>,
+  ],
+};
+
+export const Basic = () => <Component />;
